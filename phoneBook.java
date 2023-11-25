4034655679de5b596413889e8f6eeee69a40d523
Ilya <ilya.tumanow2015@yandex.ru>
2023-11-25 20:58:00 +0300

    Initial commit

---
+++ phoneBook.java
@@ -0,0 +1,24 @@
+public class phoneBook {
+    private static HashMap<String, ArrayList<Integer>> phoneBook = new HashMap<>();
+
+    public void add(String name, Integer phoneNum) {
+
+// Введите свое решение ниже
+        ArrayList<Integer> phoneNums = phoneBook.getOrDefault(name, new ArrayList<>());
+        phoneNums.add(phoneNum);
+        phoneBook.put(name, phoneNums);
+    }
+
+    public ArrayList<Integer> find(String name) {
+// Введите свое решение ниже
+        return phoneBook.getOrDefault(name, new ArrayList<>());
+
+    }
+
+    public static HashMap<String, ArrayList<Integer>> getPhoneBook() {
+// Введите свое решение ниже
+        return phoneBook;
+
+    }
+}
+
