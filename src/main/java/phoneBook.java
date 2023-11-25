e7632b5d61430ba16d89b99bfabcdaa993d8381f
Ilya <ilya.tumanow2015@yandex.ru>
2023-11-25 21:12:31 +0300

    3

--- src/main/java/phoneBook.java
+++ src/main/java/phoneBook.java
@@ -2,6 +2,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 public class phoneBook {
     private static HashMap<String, ArrayList<Integer>> phoneBook = new HashMap<>();
+    public void add(String name, Integer phoneNum) {
 
+
+        ArrayList<Integer> phoneNums = phoneBook.getOrDefault(name, new ArrayList<>());
+        phoneNums.add(phoneNum);
+        phoneBook.put(name, phoneNums);
+    }
+
+    public ArrayList<Integer> find(String name) {
+
+        return phoneBook.getOrDefault(name, new ArrayList<>());
+
+    }
+
+    public static HashMap<String, ArrayList<Integer>> getPhoneBook() {
+
+        return phoneBook;
+
+    }
 }
 
